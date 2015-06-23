1e4573fd3aa5b5a6f0c47425fc1eb20d392c20c7
Mike Walker <michael@lazerwalker.com>
2015-06-23 15:10:40 -0700

    Store messages in localStorage

--- client/src/views/ChatView.js
+++ client/src/views/ChatView.js
@@ -5,13 +5,17 @@
   }
   window.ChatView = React.createClass({
     getInitialState: function() {
-      return {
-        messages: [
+      var messages;
+      if (localStorage.messages) {
+        messages = JSON.parse(localStorage.messages);
+      } else {
+        messages = [
           {type: Type.BOT, msg: "how u doin bae? ;)"},
           {type: Type.BOT, msg: "[OPERATIONS MANUAL: Using nudity-detection algorithms, Sext Machine is programmed to feel arousal relative to the likelihood a photo has inappropriate content."},
           {type: Type.BOT, msg:"Trick the unit into being aroused by taking and sending pictures with your phone camera that aren't x-rated, but it believes are.]"}
         ]
       }
+      return { messages: messages }
     },
     render: function() {
       var messages = this.state.messages.map(function(msg) {
@@ -37,6 +41,7 @@
       var messages = this.state.messages;
       messages.push({type: Type.YOU, url: photo});
       this.setState({messages: messages})
+      localStorage.messages = JSON.stringify(messages);
     }
   });
 })();
